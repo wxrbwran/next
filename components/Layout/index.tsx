35fd8a4f381d06d3293777ca55137693205f5e19
wuxiaoran <wxrbw@qq.com>
2019-05-08 15:13:03 +0800

    feat($tsx&css): 配置ts,less,scss

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -11,7 +11,6 @@ function Layout({ children }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
         <title>Next-Antd-Scafflod</title>
-        <link rel='stylesheet' href='https://cdn.bootcss.com/antd/3.16.3/antd.css' />
       </Head>
       <Header />
       {children}
--- next.config.js
+++ next.config.js
@@ -1,42 +1,131 @@
 const path = require('path')
-const glob = require('glob')
-const webpack = require('webpack')
 const withTypescript = require('@zeit/next-typescript')
-// const _withTypescript = require('next-awesome-typescript')
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withLess = require('@zeit/next-less')
-const withPlugins = require('next-compose-plugins')
 const theme = require('./theme');
-const tsImportPluginFactory = require('ts-import-plugin');
+const cssLoaderConfig = require('@zeit/next-css/css-loader-config')
+const lessToJS = require('less-vars-to-js')
 const fs = require('fs')
 
-
-// console.log(fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8'))
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
   require.extensions['.less'] = file => {};
 }
 
-module.exports = withPlugins([
-  withCSS,
-  [withSass, {
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: '[local]_[hash:base64:6]',
-    },
-    sassLoaderOptions: {
-      data: '@import "_base.scss";',
-      includePaths: [path.resolve('./styles')],
-    },
-  }],
-  [withLess, {
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: theme, // make your antd custom effective
-    },
-  }],
-  [withTypescript, {}],
-]);
+module.exports = {
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx'],
+  webpack(config, options) {
+    if (!options.defaultLoaders) {
+      throw new Error(
+        'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+      );
+    }
+    const nextConfig = {};
+    const { dir, dev, defaultLoaders, isServer } = options;
+    const {
+      cssModules,
+      cssLoaderOptions,
+      postcssLoaderOptions,
+      lessLoaderOptions = {},
+    } = nextConfig;
+
+    console.log('options.defaultLoaders', defaultLoaders)
+
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    defaultLoaders.less = cssLoaderConfig(config, {
+      extensions: ['less'],
+      cssModules,
+      cssLoaderOptions,
+      postcssLoaderOptions,
+      dev,
+      isServer,
+      loaders: [
+        {
+          loader: 'less-loader',
+          options: lessLoaderOptions,
+        },
+      ],
+    })
+
+    config.module.rules.push({
+      test: /\.less$/,
+      exclude: /node_modules/,
+      use: options.defaultLoaders.less,
+    })
+
+    // 我们禁用了antd的cssModules
+    config.module.rules.push({
+      test: /\.less$/,
+      include: /node_modules/,
+      use: cssLoaderConfig(config, {
+        extensions: ['less'],
+        cssModules: false,
+        cssLoaderOptions: {},
+        dev,
+        isServer,
+        loaders: [
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: themeVariables,
+            },
+          },
+        ],
+      }),
+    })
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: cssLoaderConfig(config, {
+        extensions: ['scss'],
+        cssModules: true,
+        cssLoaderOptions: {
+          importLoaders: 1,
+          localIdentName: '[local]_[hash:base64:6]',
+        },
+        dev,
+        isServer,
+        loaders: [
+          {
+            loader: 'sass-loader',
+            options: {
+              data: '@import "_base.scss";',
+              includePaths: [path.resolve('./styles')],
+            },
+          },
+        ],
+      }),
+    })
+
+    if (!defaultLoaders.hotSelfAccept) {
+      if (dev && !isServer) {
+        config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          loader: 'hot-self-accept-loader',
+          include: [path.join(dir, 'pages')],
+          options: {
+            extensions: /\.(ts|tsx)$/
+          }
+        })
+      }
+    }
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: [dir],
+      exclude: /node_modules/,
+      use: defaultLoaders.babel,
+    });
+
+    if (typeof nextConfig.webpack === 'function') {
+      return nextConfig.webpack(config, options)
+    }
+
+    return config
+  },
+}
